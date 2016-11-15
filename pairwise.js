538305c4896b5211d978decdf739bfac9fe90b8b
Kieran Harding <kieranharding@gmail.com>
2016-11-14 22:02:51 -0600

    Pairwise passing all tests
    On branch master
    Changes to be committed:
    	modified:   pairwise.js

--- pairwise.js
+++ pairwise.js
@@ -1,4 +1,4 @@
-/*Given an array arr, find element pairs whose sum equal the second argument arg and return the sum of their indices.
+/* Given an array arr, find element pairs whose sum equal the second argument arg and return the sum of their indices.
 
 If multiple pairs are possible that have the same numeric elements but different indices, return the smallest sum of indices. Once an element has been used, it cannot be reused to pair with another.
 
@@ -12,6 +12,37 @@ Below we'll take their corresponding indices and add them.
 9 + 11 = 20 → Indices 1 + 2 = 3
 3 + 3 = 6 → Return 6 */
 
-function pairwise(arr, arg) {
-  return arg
+function pairwise (arr, arg) {
+  function sumArray (a, b) {
+    return a + b
+  }
+
+  function concatDistinct (arr, a, b) {
+    // Concat 2 items to the array, only if neither of them are already there.
+    if (arr.indexOf(a) < 0 && arr.indexOf(b) < 0) {
+      return arr.concat([a, b])
+    } else {
+      return arr
+    }
+  }
+
+  function findPairs (arr, n, idxs) {
+    idxs = idxs || []
+    if (arr.length < 2) return idxs
+    n = n || 0
+    var i
+    // Second half of this is redundant with concatDistinct, but saves loops
+    if (arr[0] < arg && idxs.indexOf(n) < 0) {
+      for (i = 1; i < arr.length; i++) {
+        if (arg - arr[0] === arr[i]) {
+          idxs = concatDistinct(idxs, n, n + i)
+        }
+      }
+    }
+    return findPairs(arr.slice(1), n + 1, idxs)
+  }
+
+  return findPairs(arr).reduce(sumArray, 0)
 }
+
+module.exports = pairwise
